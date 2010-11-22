92e9755eac480670315c56b2df0023e5f47aef2f
JuanJo Ciarlante <jjo@google.com>
2010-11-22 13:12:09 -0800

    remove quotes from the inline

--- jjo-gnome-setupme.sh
+++ jjo-gnome-setupme.sh
@@ -7,17 +7,17 @@ gconftool --list-type string -t list -s /desktop/gnome/peripherals/keyboard/kbd/
 while read var value;do
 	(set -x;gconftool -t string -s /apps/gnome-terminal/$var $value)
 done <<EOF
-keybindings/switch_to_tab_1 '<Control>1'
-keybindings/switch_to_tab_2 '<Control>2'
-keybindings/switch_to_tab_3 '<Control>3'
-keybindings/switch_to_tab_4 '<Control>4'
-keybindings/switch_to_tab_5 '<Control>5'
-keybindings/switch_to_tab_6 '<Control>6'
-keybindings/switch_to_tab_7 '<Control>7'
-keybindings/switch_to_tab_8 '<Control>8'
-keybindings/switch_to_tab_9 '<Control>9'
-profiles/Default/background_color '#000000000000'
-profiles/Default/foreground_color '#FFFFFFFFFFFF'
+keybindings/switch_to_tab_1 <Control>1
+keybindings/switch_to_tab_2 <Control>2
+keybindings/switch_to_tab_3 <Control>3
+keybindings/switch_to_tab_4 <Control>4
+keybindings/switch_to_tab_5 <Control>5
+keybindings/switch_to_tab_6 <Control>6
+keybindings/switch_to_tab_7 <Control>7
+keybindings/switch_to_tab_8 <Control>8
+keybindings/switch_to_tab_9 <Control>9
+profiles/Default/background_color #000000000000
+profiles/Default/foreground_color #FFFFFFFFFFFF
 profiles/Default/use_theme_colors false
 profiles/Default/use_menu_accelerators false
 profiles/Default/use_mnemonics false
