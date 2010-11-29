a393ae67e67c6ed7fee29e7890f71f4ebc4d1243
JuanJo Ciarlante <jjo@google.com>
2010-11-29 17:21:22 +0100

    added alt-fN for desktop switching, plus cleanups

--- jjo-gnome-setupme.sh
+++ jjo-gnome-setupme.sh
@@ -1,56 +1,44 @@
 #set -x
 #permanent in gnome:
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 8 6 7 4 5
-gconftool --list-type string -t list -s /desktop/gnome/peripherals/keyboard/kbd/options '[lv3	lv3:ralt_switch,ctrl	ctrl:nocaps]'
 while read var value;do
 	type=string
-	case "$value" in true|false) type=bool;; esac
-	(set -x;gconftool -t $type -s /apps/gnome-terminal/$var $value)
+	case "$var" in \#*) continue;;esac
+	case "$value" in true|false) type=bool;; [0-9]*) type=int;; esac
+	(set -x;gconftool -t $type -s $var $value)
 done <<EOF
-keybindings/switch_to_tab_1 <Control>1
-keybindings/switch_to_tab_2 <Control>2
-keybindings/switch_to_tab_3 <Control>3
-keybindings/switch_to_tab_4 <Control>4
-keybindings/switch_to_tab_5 <Control>5
-keybindings/switch_to_tab_6 <Control>6
-keybindings/switch_to_tab_7 <Control>7
-keybindings/switch_to_tab_8 <Control>8
-keybindings/switch_to_tab_9 <Control>9
-profiles/Default/use_theme_colors false
-profiles/Default/background_type solid
-profiles/Default/background_color #000000000000
-profiles/Default/foreground_color #FFFFFFFFFFFF
-profiles/Default/use_menu_accelerators false
-profiles/Default/use_mnemonics false
+/apps/gnome-terminal/keybindings/switch_to_tab_1 <Control>1
+/apps/gnome-terminal/keybindings/switch_to_tab_2 <Control>2
+/apps/gnome-terminal/keybindings/switch_to_tab_3 <Control>3
+/apps/gnome-terminal/keybindings/switch_to_tab_4 <Control>4
+/apps/gnome-terminal/keybindings/switch_to_tab_5 <Control>5
+/apps/gnome-terminal/keybindings/switch_to_tab_6 <Control>6
+/apps/gnome-terminal/keybindings/switch_to_tab_7 <Control>7
+/apps/gnome-terminal/keybindings/switch_to_tab_8 <Control>8
+/apps/gnome-terminal/keybindings/switch_to_tab_9 <Control>9
+/apps/gnome-terminal/profiles/Default/use_theme_colors false
+/apps/gnome-terminal/profiles/Default/background_type solid
+/apps/gnome-terminal/profiles/Default/background_color #000000000000
+/apps/gnome-terminal/profiles/Default/foreground_color #FFFFFFFFFFFF
+/apps/gnome-terminal/profiles/Default/use_menu_accelerators false
+/apps/gnome-terminal/profiles/Default/use_mnemonics false
+/apps/metacity/global_keybindings/switch_to_workspace_1 <Alt>F1
+/apps/metacity/global_keybindings/switch_to_workspace_2 <Alt>F2
+/apps/metacity/global_keybindings/switch_to_workspace_3 <Alt>F3
+/apps/metacity/global_keybindings/switch_to_workspace_4 <Alt>F4
+#flap it:
+/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation false
+/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation_button 3
+/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation_x_axis false
+/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation_x_axis true
+/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation_button 2
+/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation true
 EOF
-
+gconftool --list-type string -t list -s /desktop/gnome/peripherals/keyboard/kbd/options '[lv3	lv3:ralt_switch,ctrl	ctrl:nocaps]'
 
 #NOW!
 setxkbmap -option ctrl:nocaps us intl
+xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
+xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2
+xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 8 6 7 4 5
 
 exit 0
-
-DUMMY="
-     <entry>
-       <key>/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation</key>
-       <value>
--        <bool>false</bool>
-+        <bool>true</bool>
-       </value>
-     </entry>
-     <entry>
-       <key>/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint/wheel_emulation_button</key>
-       <value>
-         <int>2</int>
-       </value>
-     </entry>
-"
-D="/desktop/gnome/peripherals/TPPS@47@2@32@IBM@32@TrackPoint"
-#gconftool-2 --type bool --set $D/wheel_emulation false
-#gconftool-2 --type int  --set $D/wheel_emulation_button 3
-#gconftool-2 --type bool --set $D/wheel_emulation_x_axis false
-#gconftool-2 --type bool --set $D/wheel_emulation_x_axis true
-#gconftool-2 --type int  --set $D/wheel_emulation_button 2
-#gconftool-2 --type bool --set $D/wheel_emulation true
