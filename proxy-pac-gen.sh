a53b57f98d4c6a4947d59b6e09b86c6b559b2cc0
JuanJo Ciarlante <jjo@google.com>
2011-01-04 12:30:29 +0100

    cleanups

--- proxy-pac-gen.sh
+++ proxy-pac-gen.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 # use it as: proxy-pac-gen.sh > $HOME/etc/proxy-auto.pac , then
 # point your browser to: echo file://$HOME/etc/proxy-auto.pac
-extract_proxies() {
-  local list=($(sed -nr '/proxy:name=XROXY/s/.*host=([0-9.]+).*port=([0-9]+).*/\1:\2/p'))
+
+N_PROXYS_PER_CC=3
+get_proxies_for() {
+  local CC=$1
+  local list=($(
+    curl -s http://www.xroxy.com/proxy--Anonymous-$CC-ssl.htm|\
+      sed -nr '/proxy:name=XROXY/s/.*host=([0-9.]+).*port=([0-9]+).*/\1:\2/p'
+  ))
   local good_list
   typeset -i n=0
   for i in ${list[*]};do
-    echo "#$1: testing $i ..." >&2
+    echo "#$CC: testing $i ..." >&2
     local out=$(
       for j in {1..4};do env http_proxy=http://$i/ curl -m5 -D- -Ls http://www.google.com/ -o /dev/null || echo FAIL & done
     )
     case "$out" in
-	*HTTP/1*500*) echo "#$1: HTTP_500 $i">&2;continue;;
-	FAIL) echo "#$1: FAIL $i" >&2;continue;;
+	*HTTP/1*500*) echo "#$CC: HTTP_500 $i">&2;continue;;
+	FAIL) echo "#$CC: FAIL $i" >&2;continue;;
     esac
-    echo "#$1: OK $i" >&2
+    echo "#$CC: OK $i" >&2
     n=n+1
     good_list="$good_list PROXY $i;"
-    test $n -eq 2 && break
+    test $n -eq $N_PROXYS_PER_CC && break
   done >&2
   echo ${good_list:=DIRECT;}
 }
 
-proxy_GB=$(curl -s http://www.xroxy.com/proxy--Anonymous-GB-ssl.htm|extract_proxies GB)
-proxy_US=$(curl -s http://www.xroxy.com/proxy--Anonymous-US-ssl.htm|extract_proxies US)
+proxy_GB=$(get_proxies_for GB)
+proxy_US=$(get_proxies_for US)
 
 cat <<EOF
 function FindProxyForURL(url, host) {
