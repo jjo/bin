e5d395ead05967a0f26b28865ed9975fa7855db1
JuanJo Ciarlante <juanjosec@gmail.com>
2011-06-11 11:24:24 -0700

    minors

--- jjo-apt-get-install-mypkgs.sh
+++ jjo-apt-get-install-mypkgs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #X="x2x chromium-browser vncviewer qemu"
-apt-get install git-core vim screen build-essential flex bison man-db tcpdump mtr-tiny atop mutt jed vnc4server curl etckeeper gettext libssl-dev unzip colordiff $X
+apt-get install git-core vim screen tmux build-essential flex bison man-db tcpdump mtr-tiny atop mutt jed vnc4server curl etckeeper gettext libssl-dev unzip colordiff $X
 # additional for dev stuff (eg. openwrt)
 apt-get install libncurses-dev gawk subversion
