402b8ee9e6d6cd0583c61f30c74413d2a2e9db1e
JuanJo Ciarlante <juanjosec@gmail.com>
2011-07-29 20:37:06 +0200

    mv {,jjo-}cgroup-browsers.sh

--- cgroup-browsers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-U=${SUDO_USER:-$USER}
-CG=${U?}_browsers
-cgcreate -t $U:root -g blkio,cpu:/$CG
-cgset -r blkio.weight=100 $CG
-cgset -r    cpu.shares=10 $CG
-cgclassify  -g blkio,cpu:/$CG $( lsof -u $U -c '/chrom|npviewer/' -t )
