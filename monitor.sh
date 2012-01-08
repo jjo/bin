894bd9f5ef837ccd53257d00c5aad52c71978c42
JuanJo Ciarlante <juanjosec@gmail.com>
2012-01-08 12:24:34 +0100

    monitor.sh: cleanup, simplified

--- monitor.sh
+++ monitor.sh
@@ -2,8 +2,18 @@
 
 
 xrandr() {
-	(set -x; exec /usr/bin/xrandr "$@" || exit 1)
+	case "$dryrun" in 
+	-n) echo xrandr "$@";;
+        "") (sleep 2;set -x; exec /usr/bin/xrandr "$@" || exit 1)
+	esac
 }
+
+usage() {
+	echo -e "Usage: monitor.sh wot [-n]\n  wot:\n$(sed -r -n 's/#[%]usage//p' $0)"
+	exit 1
+}
+
+test $# -gt 0 || usage
 
 typeset -a OUT_DEV_RES=($(/usr/bin/xrandr -q|egrep -A1 '^(VGA|DP|HDMI)[0-9] conn'| awk '{ print $1 }'))
 typeset -a LCD_DEV_RES=($(/usr/bin/xrandr -q|egrep -A1 '^LVDS[0-9] conn'| awk '{ print $1 }'))
@@ -14,30 +24,29 @@ LCD_DEV=${LCD_DEV_RES[0]}
 LCD_RES=${LCD_DEV_RES[1]}
 
 echo LCD:${LCD_DEV?}@${LCD_RES?} OUT:${OUT_DEV?}@${OUT_RES?} 
-sleep 2
 
-case "$1" in
-    solo)
+wot="$1"
+dryrun="$2"
+case "$wot" in
+    solo) #%usage Live only LCD display
         xrandr --output $LCD_DEV --mode $LCD_RES --output $OUT_DEV --off
         ;;
-    pres)
-        xrandr --output $LCD_DEV --mode $PRES_MODE --output $OUT_DEV --same-as $LCD_DEV --mode $PRES_MODE
+    pres) #%usage Presentation mode 1:1
+	LCD_RES=$PRES_MODE
+	OUT_RES=$PRES_MODE
+        xrandr --output $LCD_DEV --mode $LCD_RES --output $OUT_DEV --same-as $LCD_DEV --mode $OUT_RES
         ;;
-    dual)
+    dualleft|dualright|dualup|dualdown|dualleftv|dualrightv) #%usage eg. dualleft, dualright, dualrightv (+vertical)
+	xtra=""
+	# eg: dualrightv -> right-of --rotate left (vertical external)
+	case "$wot" in *v) xtra="--rotate left";wot=${wot%v};; esac
+	where="${wot#dual}-of"
+	case $where in "up-of") where="above";; "down-of") where="below";; esac
         xrandr --output $LCD_DEV --mode $LCD_RES --output $OUT_DEV --off
-        xrandr --output $LCD_DEV --mode $LCD_RES --pos 0x0 --left-of $OUT_DEV --output $OUT_DEV --mode $OUT_RES --right-of $LCD_DEV
-        ;;
-    dualup)
-        xrandr --output $LCD_DEV --mode $LCD_RES --output $OUT_DEV --off
-        xrandr --output $LCD_DEV --mode $LCD_RES --pos 0x0 --below $OUT_DEV --output $OUT_DEV --mode $OUT_RES
-        ;;
-    dualv)
-        xrandr --output $LCD_DEV --mode $LCD_RES --output $OUT_DEV --off
-        xrandr --output $LCD_DEV --mode $LCD_RES --pos 0x0 --left-of $LCD_DEV --output $OUT_DEV --mode $OUT_RES --right-of $LCD_DEV --rotate left
+        xrandr --output $LCD_DEV --mode $LCD_RES --pos 0x0 --output $OUT_DEV --mode $OUT_RES --$where $LCD_DEV $xtra
         ;;
     *)
-        echo "Uso: monitor.sh $(sed -r -n 's/^[ \t]+([a-z0-9]+)\)/\t\1|/p' $0)"
-        exit 1
+        usage
         ;;
 esac
 
