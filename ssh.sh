d5c3941326ad5018fc1985490fac927f54af1b7c
JuanJo Ciarlante <juanjosec@gmail.com>
2012-03-23 09:04:34 -0300

    added completion hint to ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # update screen's title="hostname", from: ssh.sh "hostname".domain.foo
+#
+# very useful addition to ~/.bashrc:
+#    complete -F _ssh ssh.sh
+#   
+
 host="${@: -1}" ## last argument
 echo -ne "\ek${host%%.*}\e\\" ## host without "domain"
 ssh "$@"
