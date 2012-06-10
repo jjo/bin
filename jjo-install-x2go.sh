29e8dcdea9fbad65087b8b9935faa74676ab0e4d
JuanJo Ciarlante <juanjosec@gmail.com>
2012-06-09 21:48:07 -0300

    jjo-install-x2go.sh: use ppa:x2go/stable

--- jjo-install-x2go.sh
+++ jjo-install-x2go.sh
@@ -2,8 +2,9 @@
 set -e
 (
 dpkg -l x2goserver && exit 0
-echo deb http://x2go.obviously-nice.de/deb/ lenny main | sudo tee /etc/apt/sources.list.d/x2go.list
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C509840B96F89133
+#echo deb http://x2go.obviously-nice.de/deb/ lenny main | sudo tee /etc/apt/sources.list.d/x2go.list
+#sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C509840B96F89133
+sudo add-apt-repository ppa:x2go/stable
 sudo apt-get update
 sudo apt-get install x2goserver sqlite # core stuff
 sudo apt-get install gnome-session x2gognomebindings # useful for gUI
