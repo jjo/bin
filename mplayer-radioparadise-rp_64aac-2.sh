c84e6e129ddbdff6fbba6099c6a71f9b4773b77b
JuanJo Ciarlante <juanjosec@gmail.com>
2012-08-31 10:39:56 -0300

    add mplayer scriptlets for my inet radios, +cleanup

---
+++ mplayer-radioparadise-rp_64aac-2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mplayer "$@" -playlist http://www.radioparadise.com/musiclinks/rp_64aac-2.m3u
--- mplayer-t23-tvout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-GEOM="-geometry 667x404-72+9"
-VF="-vf expand=0:-90:0:0"
-VO="-vo sdl"
-set -x
-exec mplayer $VF $VO $GEOM "$@"
-
