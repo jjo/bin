f52673b43034e12cc2de3b0daf284e84f0a20f74
JuanJo Ciarlante <juanjosec@gmail.com>
2013-01-16 17:09:59 -0300

    simplify jjo-dolar-rt.sh

--- jjo-dolar-rt.sh
+++ jjo-dolar-rt.sh
@@ -8,7 +8,7 @@ echo "oficial.compra ${dolar[2]}"
 echo "oficial.venta ${dolar[3]}"
 }
 ambito() {
-	curl -s http://www.ambito.com/economia/mercados/monedas/dolar/|egrep -o '(OFICIAL|INFORMAL)|[0-9]+,[0-9]+.*(COMPRA|VENTA)'|xargs -l3|sed -rn 's/,/./g;s/(^[A-Z]+) ([0-9.]+)<.*(COMPRA).* ([0-9.]+)<.*(VENTA)/\L\1.\L\3 \2\n\L\1.\L\5 \4/gp'
+	curl -s http://www.ambito.com/economia/mercados/monedas/dolar/|egrep -o '(OFICIAL|INFORMAL)|[0-9]+,[0-9]+.*(COMPRA|VENTA)'|xargs -l3|sed -rn 's/(^[A-Z]+) ([0-9,]+)<.*(COMPRA).* ([0-9,]+)<.*(VENTA)/\L\1.\L\3 \2\n\L\1.\L\5 \4/gp'
 }
 #FUNC=eldolarblue_net
 : ${FUNC:=ambito}
