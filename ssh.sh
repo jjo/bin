770811aa2ea51ea5262cb5973da09c30db62cc34
JuanJo Ciarlante <juanjosec@gmail.com>
2013-10-03 00:16:59 -0300

    ssh.sh: simple MOSH_GATE (mosh--mosh+ssh--ssh) support

--- ssh.sh
+++ ssh.sh
@@ -5,7 +5,13 @@
 #    complete -F _ssh ssh.sh
 #   
 
+
 host="${@: -1}" ## last argument
 [[ $TERM = screen ]] && echo -ne "\ek${host%%.[a-z]*[a-z]}\e\\" ## host without "domain"
-ssh "$@"
+if [ -n "$MOSH_GATE" ];then
+	echo "Using MOSH_GATE: $MOSH_GATE"
+	mosh -- $MOSH_GATE bash -ic "ssh $@"
+else
+	ssh -C "$@"
+fi
 [[ $TERM = screen ]] && echo -ne "\ek${SHELL##*/}\e\\"
