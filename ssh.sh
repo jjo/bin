7d8fa5c028cf62481a590735b16f26f4fea9fea4
JuanJo Ciarlante <juanjosec@gmail.com>
2013-10-04 12:39:13 -0300

    some ssh.sh touches for "mosh -A"

--- ssh.sh
+++ ssh.sh
@@ -8,9 +8,11 @@
 
 host="${@: -1}" ## last argument
 [[ $TERM = screen ]] && echo -ne "\ek${host%%.[a-z]*[a-z]}\e\\" ## host without "domain"
-if [ -n "$MOSH_GATE" ];then
+if [ -n "$MOSH_GATE" -a "$host" != "$MOSH_GATE" ];then
 	echo "Using MOSH_GATE: $MOSH_GATE"
-	mosh -- $MOSH_GATE bash -ic "ssh $@"
+	# -A requires mosh built from
+	# https://github.com/rinne/mosh/tree/ssh-agent-forwarding
+	mosh -A -- $MOSH_GATE bash -ic "ssh $@"
 else
 	ssh -C "$@"
 fi
