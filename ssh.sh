86e537daf7fa2f753eea47b8dcf00eb1667572f2
JuanJo Ciarlante <juanjosec@gmail.com>
2014-03-03 15:01:39 -0300

    ssh.sh: add -o ServerAliveInterval=300

--- ssh.sh
+++ ssh.sh
@@ -3,17 +3,17 @@
 #
 # very useful addition to ~/.bashrc:
 #    complete -F _ssh ssh.sh
-#   
+#
 
-
+SSH_OPTS='-o ServerAliveInterval=300 -C'
 host="${@: -1}" ## last argument
 [[ $TERM = screen ]] && echo -ne "\ek${host%%.[a-z]*[a-z]}\e\\" ## host without "domain"
 if [ -n "$MOSH_GATE" -a "$host" != "$MOSH_GATE" ];then
 	echo "Using MOSH_GATE: $MOSH_GATE"
 	# -A requires mosh built from
 	# https://github.com/rinne/mosh/tree/ssh-agent-forwarding
-	mosh -A -- $MOSH_GATE bash -ic "ssh $@"
+	mosh -A -- $MOSH_GATE bash -ic "ssh ${SSH_OPTS} $@"
 else
-	ssh -C "$@"
+	ssh ${SSH_OPTS} "$@"
 fi
 [[ $TERM = screen ]] && echo -ne "\ek${SHELL##*/}\e\\"
