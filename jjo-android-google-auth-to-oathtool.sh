8a6a22fb32f1d752da4f8f16074261d0d01b32d9
JuanJo Ciarlante <juanjosec@gmail.com>
2014-03-13 17:40:22 -0300

    jjo-android-google-auth-to-oathtool.sh: add some doco

--- jjo-android-google-auth-to-oathtool.sh
+++ jjo-android-google-auth-to-oathtool.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # Print oathtool commands to stdout that do produce same OTPs
-# as Google Authenticator App - requires rooted android
+# as Google Authenticator App, by steal^Wusing its using keys.
+# Requires rooted android.
+#
+# Example (obfuscated) output:
+# oathtool --totp -b XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 	# Google:[...]@[...]
+# oathtool -c 7   -b XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 	# UbuntuSSO/[...]@[...]
+# oathtool --totp -b XXXXXXXXXXXXXXXXXXXXXXXXXX 	# Dropbox:[...]@[...]
+#
+# You may want to backup these :)
+#   ./jjo-android-google-auth-to-oathtool.sh | gpg -o google-auth-oathtool.bak.gpg
+# ... and/or backup the output from:
+# adb shell su root sqlite3 /data/data/com.google.android.apps.authenticator2/databases/databases .dump
+
 adb shell su root sqlite3 /data/data/com.google.android.apps.authenticator2/databases/databases \
    "select email, secret, counter, type from accounts" | \
    awk -v FS='|' ' {
