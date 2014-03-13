2ba6a2aa4a083a1d187648fe96fca364a08180ee
JuanJo Ciarlante <juanjosec@gmail.com>
2014-03-13 10:09:41 -0300

    minors

--- jjo-inet-pktloss.sh
+++ jjo-inet-pktloss.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -v
-elinks -no-numbering -no-references internetpulse\.net/?Metric=PL |egrep --color=no -e 'Dest|Gener' -e "NTT.*XO" -e " 0 .* 0  "
+elinks -no-numbering -no-references http://internetpulse.net/?Metric=PL |egrep --color=no -e 'Dest|Gener' -e "NTT.*XO" -e " 0 .* 0  "
