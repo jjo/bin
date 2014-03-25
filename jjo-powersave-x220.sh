aee8e8d0691a9df548e3b09535853719d9d67eb3
JuanJo Ciarlante <juanjosec@gmail.com>
2014-03-24 22:21:58 -0300

    add some settings from powertop tunables

--- jjo-powersave-x220.sh
+++ jjo-powersave-x220.sh
@@ -4,8 +4,12 @@ set -v
 sysctl -w vm.laptop_mode=5
 sysctl -w vm.dirty_writeback_centisecs=1500
 for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo ondemand > $i;done
-echo 1         > /sys/module/snd_hda_intel/parameters/power_save
+echo '1' > /sys/module/snd_hda_intel/parameters/power_save
 for i in /sys/class/scsi_host/host*/link_power_management_policy; do echo min_power > $i;done
+for i in /sys/bus/{pci,usb}/devices/*/power/control;do echo auto > $i;done
+echo '0' > '/proc/sys/kernel/nmi_watchdog';
+
+
 iwconfig wlan0 power on
 iwconfig wlan0 power timeout 500ms
 #for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 1 > $i; done
