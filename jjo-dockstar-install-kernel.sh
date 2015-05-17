ed1b844bf565b55a145eab8b88ef30957a5e1677
JuanJo Ciarlante <juanjosec@gmail.com>
2015-05-17 12:12:38 -0300

    added jjo-dockstar-install-kernel.sh

---
+++ jjo-dockstar-install-kernel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+# Install kernel, initrd for dockstar u-boot, see
+# http://forum.doozan.com/read.php?2,12096
+set -xeu
+V=${1:-missing version, eg: 4.0.0-kirkwood-tld-2}
+DTS=dts/kirkwood-dockstar.dtb
+ls -la zImage-$V vmlinuz-$V initrd.img-$V $DTS
+cp -a zImage-$V  zImage.fdt
+cat $DTS  >> zImage.fdt
+mkimage -A arm -O linux -T kernel  -C none -a 0x00008000 -e 0x00008000 -n Linux-$V     -d zImage.fdt    uImage
+mkimage -A arm -O linux -T ramdisk -C gzip -a 0x00000000 -e 0x00000000 -n initramfs-$V -d initrd.img-$V uInitrd
+rm -f zImage.fdt
+ls -la uImage uInitrd
