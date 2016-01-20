8735fc8d0c3accd394431f8349e0919904f0c2c6
JuanJo Ciarlante <juanjosec@gmail.com>
2016-01-20 10:47:53 -0300

    [jjo] update mplayer-mdzol.sh to parse player.js for media URL

--- mplayer-mdzol.sh
+++ mplayer-mdzol.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
 # http://www.universomdz.com/home.html
 #exec mplayer -playlist $@ http://207.198.106.33:1935/mdzradio/default.stream/playlist.m3u8
-exec nvlc http://207.198.106.33:1935/mdzradio/default.stream/playlist.m3u8
+#exec nvlc http://207.198.106.33:1935/mdzradio/default.stream/playlist.m3u8
+URL=$(wget -qO- http://latinstatic.edgesuite.net/radio-mdz/player.js|sed -rn 's,\\,,g;/video-link/s/.*(http[^"]+)".*/\1/p')
+echo "URL=$URL"
+exec nvlc "$URL"
