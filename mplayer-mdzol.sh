adf7ed92c1b7ed48924a4336dc29a2e7a5b808dc
JuanJo Ciarlante <juanjosec@gmail.com>
2016-05-20 09:02:21 -0300

    [jjo] mplayer-mdzol.sh: add --save

--- mplayer-mdzol.sh
+++ mplayer-mdzol.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
-# http://www.universomdz.com/home.html
-#exec mplayer -playlist $@ http://207.198.106.33:1935/mdzradio/default.stream/playlist.m3u8
-#exec nvlc http://207.198.106.33:1935/mdzradio/default.stream/playlist.m3u8
 URL=$(wget -qO- http://latinstatic.edgesuite.net/radio-mdz/player.js|sed -rn 's,\\,,g;/video-link/s/.*(http[^"]+)".*/\1/p')
 echo "URL=$URL"
-exec nvlc "$URL"
+extra=""
+if [[ "$1" == --save ]];then
+    extra="--sout #std{access=file,mux=raw,dst=$2}"
+fi
+set -x
+exec cvlc $extra "$URL"
