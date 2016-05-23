bfea5a852be2657637d7f521c25e484f19609cff
JuanJo Ciarlante <juanjosec@gmail.com>
2016-05-23 17:16:05 -0300

    [jjo] mplayer-mdzol.sh: updates

--- mplayer-mdzol.sh
+++ mplayer-mdzol.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 #URL=$(wget -qO- http://latinstatic.edgesuite.net/radio-mdz/player.js|sed -rn 's,\\,,g;/video-link/s/.*(http[^"]+)".*/\1/p')
-URL=$(wget -qO- www.mdzradio.com/player.php|egrep androidButton|sed -rn 's|.*"(http://.*/playlist.m3u8)".*|\1|p')
+#URL=$(wget -qO- www.mdzradio.com/player.php|egrep androidButton|sed -rn 's|.*"(http://.*/playlist.m3u8)".*|\1|p')
+URL=$(wget -qO- http://latinstatic.edgesuite.net/radio-mdz/js/classes/JwPlayer2.js | sed -rn '/jwplayer.*load/s|.*(http:[^"]+)".*|\1|p')
 echo "URL=$URL"
 extra=""
 VLC=nvlc
