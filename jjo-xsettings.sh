ba79b068b43de38ecf7cf7954c7a87d6d96829db
JuanJo Ciarlante <juanjosec@gmail.com>
2016-08-23 22:29:02 +0200

    jjo-xsettings.sh update

--- jjo-xsettings.sh
+++ jjo-xsettings.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# Setup vertical mouse
-xinput set-button-map "$(xinput -list |sed -nr 's/.*Kingsis.*id=([0-9]+).*/\1/p')" 1 2 3 4 5 6 7 8 9 10 11 12 13 14
 setxkbmap -option ctrl:nocaps us altgr-intl
-#disable thinkpad trackpad
-trackpad_id=$(xinput list|sed -nr '/Synaptics.TouchPad/s/.*id=([0-9]+).*/\1/p')
-test -n "$trackpad_id" && xinput set-prop $trackpad_id "Device Enabled" 0
+# Vertical mouse: setup button-map
+vmouse_id="$(xinput -list |sed -nr 's/.*Kingsis.*id=([0-9]+).*/\1/p')"
+test -n "$vmouse_id" && (set -x;xinput set-button-map "$vmouse_id" 1 2 3 4 5 6 7 8 9 10 11 12 13 14)
+# TrackPoint: bounce it - w/ ubuntu LTS 16.04 around ~Aug/2016 started to come out of suspend w/o buttons working [-(
+trackpoint_id=$(xinput list|sed -nr '/TrackPoint/s/.*id=([0-9]+).*/\1/p')
+test -n "$trackpoint_id" && (set -x; xinput disable $trackpoint_id ; xinput enable $trackpoint_id)
+sleep 0.2 #(?), needed anyway to effectively disable ->
+# Touchpad: disable
+touchpad_id=$(xinput list|sed -nr '/Synaptics.TouchPad/s/.*id=([0-9]+).*/\1/p')
+test -n "$touchpad_id" && (set -x;xinput set-prop "$touchpad_id" "Device Enabled" 0; xinput disable "$touchpad_id")
