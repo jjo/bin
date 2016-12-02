e38f2fa062565d5a3b386f063a23a65fb33ed881
JuanJo Ciarlante <juanjosec@gmail.com>
2016-12-02 16:04:52 -0300

    [jjo] add sox to my pkgs (for "play" cmd from notify-send.sh)

--- jjo-apt-get-install-mypkgs.sh
+++ jjo-apt-get-install-mypkgs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #X="x2x chromium-browser vncviewer qemu"
-apt-get install git-core vim screen tmux irssi build-essential flex bison man-db mtr-tiny mutt vnc4server curl etckeeper gettext libssl-dev unzip colordiff terminator mplayer vlc-nox $X
+apt-get install git-core vim screen tmux irssi build-essential flex bison man-db mtr-tiny mutt vnc4server curl etckeeper gettext libssl-dev unzip colordiff terminator mplayer vlc-nox sox $X
 apt-get install tcpdump ngrep nmap sysstat sysdig iftop atop htop iotop
 apt-get install golang-go
 # additional for dev stuff (eg. openwrt)
