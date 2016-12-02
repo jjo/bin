4533807d515362ca0bec4371d5373f42c934d24f
JuanJo Ciarlante <juanjosec@gmail.com>
2016-12-02 13:19:29 -0300

    [jjo] jjo-apt-get-install-mypkgs.sh update

--- jjo-apt-get-install-mypkgs.sh
+++ jjo-apt-get-install-mypkgs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #X="x2x chromium-browser vncviewer qemu"
-apt-get install git-core vim screen tmux build-essential flex bison man-db tcpdump mtr-tiny atop mutt vnc4server curl etckeeper gettext libssl-dev unzip colordiff terminator mplayer vlc-nox $X
+apt-get install git-core vim screen tmux irssi build-essential flex bison man-db mtr-tiny mutt vnc4server curl etckeeper gettext libssl-dev unzip colordiff terminator mplayer vlc-nox $X
+apt-get install tcpdump ngrep nmap sysstat sysdig iftop atop htop iotop
 apt-get install golang-go
 # additional for dev stuff (eg. openwrt)
 # apt-get install libncurses-dev gawk subversion
