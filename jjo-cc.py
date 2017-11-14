2e4b979e8ab6c17f431d6005aeaddfed5db60e74
JuanJo Ciarlante <juanjosec@gmail.com>
2017-11-14 16:18:44 -0300

    jjo-cc.py: add XRP

--- jjo-cc.py
+++ jjo-cc.py
@@ -6,7 +6,9 @@ from jinja2 import Template
 
 # Q&D from several sources
 POLONIEX_URL = "https://poloniex.com/public"
-POLONIEX_COINS = "BTC BCH ZEC ETH DASH OMNI XMR NOTE DGB DOGE POT LSK".split()
+POLONIEX_COINS = (
+    "BTC BCH ZEC ETH DASH OMNI XMR "
+    "NOTE DGB DOGE POT LSK XRP").split()
 HITBTC_URL = "https://api.hitbtc.com/api/1/public/%sBTC/ticker"
 HITBTC_COINS = "TKN BTG".split()
 
