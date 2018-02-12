c9c7fbc0312d2a5e3966bce531fd7d32adafff36
JuanJo Ciarlante <juanjosec@gmail.com>
2018-02-12 10:17:41 -0300

    monitor.sh related fixes (eDP-?[0-9] naming)

--- monitor.sh
+++ monitor.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 xrandr() {
-	case "$dryrun" in
-	-n) echo xrandr "$@";;
+    case "$dryrun" in
+    -n) echo xrandr "$@";;
         "") (sleep 1;set -x; exec /usr/bin/xrandr "$@" || exit 1)
-	esac
+    esac
 }
 
 usage() {
-	echo -e "Usage: monitor.sh wot [-n]\n  wot:\n$(sed -r -n 's/#[%]usage//p' $0)"
-	exit 1
+    echo -e "Usage: monitor.sh wot [-n]\n  wot:\n$(sed -r -n 's/#[%]usage//p' $0)"
+    exit 1
 }
 
 test $# -gt 0 || usage
 
 typeset -a OUT_DEV_RES=($(/usr/bin/xrandr -q|egrep -A1 '^(VGA|DP-1|DP-2|HDMI)-?[0-9] conn'| awk '/^[^-]/{ print $1 }'))
-typeset -a LCD_DEV_RES=($(/usr/bin/xrandr -q|egrep -A1 '^(LVDS[0-9]|eDP-[0-9]) conn'| awk '{ print $1 }'))
+typeset -a LCD_DEV_RES=($(/usr/bin/xrandr -q|egrep -A1 '^(LVDS[0-9]|eDP-?[0-9]) conn'| awk '{ print $1 }'))
 PRES_MODE=1024x768
 OUT_DEV1=${OUT_DEV_RES[0]}
 OUT_RES1=${OUT_DEV_RES[1]}
@@ -23,14 +23,15 @@ OUT_DEV2=${OUT_DEV_RES[2]}
 OUT_RES2=${OUT_DEV_RES[3]}
 LCD_DEV=${LCD_DEV_RES[0]}
 LCD_RES=${LCD_DEV_RES[1]}
+: ${LCD_DEV:?} ${LCD_RES:?}
 
 case "$1" in
-	-n) dryrun="$1"; shift;;
+    -n) dryrun="$1"; shift;;
 esac
 wot="$1"
 shift; test -n "$1" && LCD_RES="$1"
-shift; test -n "$1" && OUT_RES1="$1"
-shift; test -n "$1" && OUT_RES2="$1"
+shift; test -n "$1" && OUT_RES1="$2"
+shift; test -n "$1" && OUT_RES2="$3"
 echo LCD:${LCD_DEV?}@${LCD_RES?} OUT:${OUT_DEV1?}@${OUT_RES1?} ${OUT_DEV2:+${OUT_DEV2}@${OUT_RES2}}
 case "$wot" in
     solo)    #%usage LCD display only
@@ -40,56 +41,67 @@ case "$wot" in
     soloext) #%usage External output only
         xrandr ${OUT_DEV1:+--output $OUT_DEV1 --off} ${OUT_DEV2:+--output $OUT_DEV2 --off}
         xrandr --output $OUT_DEV1 --mode $OUT_RES1 ${LCD_DEV:+--output $LCD_DEV --off}
-	;;
+        ;;
     pres*)   #%usage Presentation mode 1:1
-	LCD_RES=$PRES_MODE
-	OUT_RES1=$PRES_MODE
+        LCD_RES=$PRES_MODE
+        OUT_RES1=$PRES_MODE
         xrandr --output $LCD_DEV --mode $LCD_RES --output $OUT_DEV1 --same-as $LCD_DEV --mode $OUT_RES1
         ;;
     dualleft|dualright|dualup|dualdown|dualleftv|dualrightv) #%usage dual<ext_position>
-	xtra=""
-	# eg: dualrightv -> right-of --rotate left (vertical external)
-	case "$wot" in *v) xtra="--rotate left";wot=${wot%v};; esac
-	where="${wot#dual}-of"
-	case $where in "up-of") where="above";; "down-of") where="below";; esac
-        xrandr ${OUT_DEV1:+--output $OUT_DEV1 --off} ${OUT_DEV2:+--output $OUT_DEV2 --off} --output $LCD_DEV --mode $LCD_RES 
-        xrandr --output $LCD_DEV --mode $LCD_RES --pos 0x0 --output $OUT_DEV1 --mode $OUT_RES1 --$where $LCD_DEV $xtra
+        : ${OUT_DEV1:?} ${OUT_RES1:?}
+        xtra=""
+        # eg: dualrightv -> right-of --rotate left (vertical external)
+        case "$wot" in *v) xtra="--rotate left";wot=${wot%v};; esac
+        where="${wot#dual}-of"
+        case $where in "up-of") where="above";; "down-of") where="below";; esac
+            xrandr ${OUT_DEV1:+--output $OUT_DEV1 --off} ${OUT_DEV2:+--output $OUT_DEV2 --off} --output $LCD_DEV --mode $LCD_RES 
+            xrandr --output $LCD_DEV --mode $LCD_RES --pos 0x0 --output $OUT_DEV1 --mode $OUT_RES1 --$where $LCD_DEV $xtra
         ;;
     h3v2)   #%usage Dual Horiz(left) Vert(right)
-	[[ $LCD_DEV =~ eDP-[0-9] ]] || \
+        : ${OUT_DEV1:?} ${OUT_RES1:?}
+        : ${OUT_DEV2:?} ${OUT_RES2:?}
+        xtra=""
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] || \
             xrandr --output $LCD_DEV --off
-        xrandr --output $OUT_DEV2 --primary
-	[[ $LCD_DEV =~ eDP-[0-9] ]] && \
-	    xrandr --output $LCD_DEV --below $OUT_DEV2 --mode $LCD_RES
-        xrandr --output $OUT_DEV1 --right-of $OUT_DEV2 --rotate right
-	;;
+            xrandr --output $OUT_DEV2 --primary
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] && \
+            xrandr --output $LCD_DEV --below $OUT_DEV2 --mode $LCD_RES
+            xrandr --output $OUT_DEV1 --right-of $OUT_DEV2 --rotate right
+        ;;
     h2v3)   #%usage Dual Horiz(left) Vert(right)
-	[[ $LCD_DEV =~ eDP-[0-9] ]] || \
+        : ${OUT_DEV1:?} ${OUT_RES1:?}
+        : ${OUT_DEV2:?} ${OUT_RES2:?}
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] || \
             xrandr --output $LCD_DEV --off
-        xrandr --output $OUT_DEV1 --primary
-	[[ $LCD_DEV =~ eDP-[0-9] ]] && \
-	    xrandr --output $LCD_DEV --below $OUT_DEV1 --mode $LCD_RES
-        xrandr --output $OUT_DEV2 --right-of $OUT_DEV1 --rotate right
-	;;
+            xrandr --output $OUT_DEV1 --primary
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] && \
+            xrandr --output $LCD_DEV --below $OUT_DEV1 --mode $LCD_RES
+            xrandr --output $OUT_DEV2 --right-of $OUT_DEV1 --rotate right
+        ;;
     h2h3)   #%usage Dual Horiz(left) Horiz(right)
-	[[ $LCD_DEV =~ eDP-[0-9] ]] || \
+        : ${OUT_DEV1:?} ${OUT_RES1:?}
+        : ${OUT_DEV2:?} ${OUT_RES2:?}
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] || \
             xrandr --output $LCD_DEV --off
-        xrandr --output $OUT_DEV1 --primary
-	[[ $LCD_DEV =~ eDP-[0-9] ]] && \
-	    xrandr --output $LCD_DEV --below $OUT_DEV1 --mode $LCD_RES
-        xrandr --output $OUT_DEV2 --right-of $OUT_DEV1
-	;;
+            xrandr --output $OUT_DEV1 --primary
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] && \
+            xrandr --output $LCD_DEV --below $OUT_DEV1 --mode $LCD_RES
+            xrandr --output $OUT_DEV2 --right-of $OUT_DEV1
+        ;;
     h3h2)   #%usage Dual Horiz(left) Horiz(right)
-	[[ $LCD_DEV =~ eDP-[0-9] ]] || \
+        : ${OUT_DEV1:?} ${OUT_RES1:?}
+        : ${OUT_DEV2:?} ${OUT_RES2:?}
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] || \
             xrandr --output $LCD_DEV --off
-        xrandr --output $OUT_DEV2 --primary
-	[[ $LCD_DEV =~ eDP-[0-9] ]] && \
-	    xrandr --output $LCD_DEV --below $OUT_DEV2 --mode $LCD_RES
-        xrandr --output $OUT_DEV1 --right-of $OUT_DEV2
-	;;
+            xrandr --output $OUT_DEV2 --primary
+        [[ $LCD_DEV =~ eDP-?[0-9] ]] && \
+            xrandr --output $LCD_DEV --below $OUT_DEV2 --mode $LCD_RES
+            xrandr --output $OUT_DEV1 --right-of $OUT_DEV2
+        ;;
     *)
         usage
         ;;
 esac
 
 exit 0
+# vim: sw=4 ts=4 si et
