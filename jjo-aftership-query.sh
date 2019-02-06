12c92a01cd5ef8ead346b837c97bfbf60c6163be
JuanJo Ciarlante <juanjosec@gmail.com>
2019-02-06 10:08:35 -0300

    rm jjo-aftership-query.sh

--- jjo-aftership-query.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. ~/etc/aftership.key
-: ${AFTERSHIP_KEY:?}
-FILTER='select(.tag!="Delivered")|'
-MANY=""
-[[ $1 == -a ]] && FILTER=""
-[[ $1 == -1 ]] && MANY=-1
-curl -sH "aftership-api-key: ${AFTERSHIP_KEY:?}" https://api.aftership.com/v4/trackings|\
-jq -r '.data.trackings[]|'"${FILTER}"'("==[ " + .tracking_number + " ][ " + .tag + " ] " + .title),(.checkpoints['"${MANY}"']|("  " + .checkpoint_time + " :  " + .message))'
