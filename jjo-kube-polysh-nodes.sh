53b8e3a69ef9b3168442094fd24d87cddc572572
JuanJo Ciarlante <juanjosec@gmail.com>
2019-08-21 08:41:55 -0300

    general updates

--- jjo-kube-polysh-nodes.sh
+++ jjo-kube-polysh-nodes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NODES=$(kubectl get node -ojsonpath={.items[*].status.addresses[0].address})
 set -x
-~/polysh/run.py --user ubuntu ${NODES}
+~/polysh/run.py --user admin ${NODES}
--- jjo-run1.sh
+++ jjo-run1.sh
@@ -5,6 +5,9 @@
 # Run a single X11 instance of a given application at cmdline
 # e.g.: jjo-run1.sh chromium-browser ...
 
+# Using mate, need to set XDG_CURRENT_DESKTOP to fix e.g.
+# `gnome-control-center network` when called from google-chrome
+export XDG_CURRENT_DESKTOP=GNOME
 which wmctrl >/dev/null || { echo "Missing wmctrl - install it."; exit 1 ;}
 wmctrl -x -R "${1:?missing cmd args ...}" || {
 	cd && exec ${SHELL} -c "$@"
