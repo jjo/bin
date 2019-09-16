949f2b29a844bc5a1557339875000c63f2e5459b
JuanJo Ciarlante <juanjosec@gmail.com>
2019-09-16 11:59:19 -0300

    use jsonnetfmt

--- jjo-jsonnet-fmt.sh
+++ jjo-jsonnet-fmt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 FMT="--indent 2 --string-style d --comment-style s --no-pad-arrays --pad-objects --pretty-field-names"
-jsonnet fmt $FMT "$@" && echo OK || echo FAIL
+jsonnetfmt $FMT "$@" && echo OK || echo FAIL
