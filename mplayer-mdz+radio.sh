ce65494f174645430e510cc71fe985da81715481
JuanJo Ciarlante <juanjosec@gmail.com>
2021-05-19 08:48:35 -0300

    reimplement mplayer-mdz+radio.sh

--- mplayer-mdz+radio.sh
+++ mplayer-mdz+radio.sh
@@ -1,1 +1,17 @@
-mplayer-tunein.sh
+#!/bin/bash
+SAVE=""
+while true; do
+    case "$1" in
+        -s | --save) SAVE="$2"; shift 2;;
+        --) shift; break;;
+        *) break;;
+    esac
+done
+
+echo SAVE=$SAVE
+URI=https://latamstreaming-live-os.akamaized.net/live_passthrough/ammdz/chunks.m3u8
+
+case "$SAVE" in
+    "") echo vlc -I ncurses "${URI}";;
+    *)  vlc -I dummy --sout="file/ogg:${SAVE}" "${URI}";;
+esac
