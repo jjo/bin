0d6a94a6e3884b39602629ab74a77769a812fef4
JuanJo Ciarlante <juanjosec@gmail.com>
2022-01-07 11:23:02 -0300

    rm jjo-pdf-compress.sh, already as jjo-pdf-to-ebook.sh

--- jjo-pdf-compress.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IN="${1:?missing input file}"
-OUT="${2:?missing input file}"
-set -x
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r75 -sOutputFile="${OUT}" "${IN}"
