24ed79efdab39f4729d2c2a2348af9ecf0e68ef0
JuanJo Ciarlante <juanjosec@gmail.com>
2022-06-07 11:56:43 -0300

    adjust mplayer scriptlet

--- mplayer-mdz+radio.sh
+++ mplayer-mdz+radio.sh
@@ -12,7 +12,11 @@ echo SAVE=$SAVE
 #URI=https://latamstreaming-live-os.akamaized.net/live_passthrough_static/ammdz/playlist.m3u8
 URI=http://lino.lsdlive.com/mdz.mp3
 
+case $(uname -s) in
+    Darwin) PLAYER="vlc -I ncurses";;
+    *) PLAYER="mplayer -prefer-ipv4";;
+esac
 case "$SAVE" in
-    "") exec vlc -I ncurses "${URI}";;
+    "") exec ${PLAYER} "${URI}";;
     *)  for i in {01..99};do sleep 0.1 || break; vlc -I dummy --sout="file/ogg:${SAVE%.ogg}-${i}.ogg" "${URI}"; done;;
 esac
