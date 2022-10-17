c704b7bbeadb49dc4dae9b33f7d371c38876c72a
JuanJo Ciarlante <juanjosec@gmail.com>
2022-10-16 20:02:45 -0500

    rework jjo-wayland-browser.sh

--- jjo-chrome.sh
+++ jjo-chrome.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-# Wayland wrappers for browsers to get pinch-to-zoom
-# Issue: only works on main screen, for (multi-screen setups) (?)
-# Issue: fails to share screen in g/meet
-XTRA=""
-case ${XDG_SESSION_TYPE:?} in
-    wayland) XTRA="--enable-features=UseOzonePlatform --ozone-platform=wayland";;
-esac
-exec google-chrome-stable ${XTRA} "${@}"
+jjo-wayland-browser.sh
---
+++ jjo-wayland-browser.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Wayland wrappers for browsers to get pinch-to-zoom
+XTRA=""
+BROWSER=""
+case ${XDG_SESSION_TYPE:?} in
+    wayland)
+        CHROME_XTRA="--enable-features=UseOzonePlatform --ozone-platform=wayland"
+        export MOZ_ENABLE_WAYLAND=1 MOZ_USE_XINPUT2=1
+        ;;
+esac
+
+case "$0" in
+    *brave*) BROWSER=brave-browser; XTRA="${CHROME_XTRA}";;
+    *chrome-beta*) BROWSER=google-chrome-beta; XTRA="${CHROME_XTRA}";;
+    *chrome*) BROWSER=google-chrome; XTRA="${CHROME_XTRA}";;
+    *chromium*) BROWSER=chromium-browser; XTRA="${CHROME_XTRA}";;
+    *firefox*) BROWSER=firefox; XTRA="";;
+esac
+exec ${BROWSER:?} ${XTRA} "${@}"
