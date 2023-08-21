ace9fce97d9cb0800c01685fbab46bcc72724390
JuanJo Ciarlante <juanjosec@gmail.com>
2023-08-21 11:46:44 -0300

    update jjo-pdf-to-ebook.sh
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- jjo-pdf-to-ebook.sh
+++ jjo-pdf-to-ebook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 infile=${1:?missing infile.pdf}
 outfile=${2:?missing outfile.pdf}
-test -f ${outfile:?} && echo "ERROR: ${outfile} already exists" && exit 1
+test -f "${outfile:?}" && echo "ERROR: ${outfile} already exists" && exit 1
 set -x
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="${outfile:?}" "${infile:?}"
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="${outfile:?}.tmp" "${infile:?}" && mv "${outfile}.tmp" "${outfile}" || rm -f "${outfile}.tmp"
