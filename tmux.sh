996c6bc4eeb9ba24f9214f01be3cce0990f8aa40
JuanJo Ciarlante <juanjosec@gmail.com>
2026-01-01 14:27:02 -0300

    tmux.sh: adjust for NUM=3,5,7,10(default)
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- tmux.sh
+++ tmux.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-export SHELL=/usr/bin/zsh
+export SHELL=$(which zsh) || exit 1
 create_window() {
   local n=$1
   local command=$2
   [ "$n" != 0 ] && tmux new-window -t "$n" -d
   tmux send-keys -t "$n" "$command" C-m
 }
+NUM=${1:-10}
+IDX=$((NUM-1))
+# LIMITN are indexed based
+case "${NUM}" in
+    10) LIMIT1=4; LIMIT2=6 ; LIMIT3=8 ;;
+    7) LIMIT1=3; LIMIT2=4 ; LIMIT3=5 ;;
+    5) LIMIT1=2; LIMIT2=3 ; LIMIT3=4 ;;
+    3) LIMIT1=0; LIMIT2=1 ; LIMIT3=2 ;;
+    default) echo "Unsupported number of window indexes: ${NUM}" ; exit 1
+    ;;
+esac
 tmux new-session -d
-for i in {0..8};do
+for i in $(seq 0 ${IDX});do
     case "$i" in
-        [0-4]) create_window "$i" "ctx w" ;;
-        [5-6]) create_window "$i" "ctx p" ;;
-        [7-8]) create_window "$i" "ctx u" ;;
+        [0-${LIMIT1}]) create_window "$i" "ctx w" ;;
+        [$((LIMIT1+1))-${LIMIT2}]) create_window "$i" "ctx p" ;;
+        [$((LIMIT2+1))-${LIMIT3}]) create_window "$i" "ctx u" ;;
     esac
 done
 exec tmux attach-session
